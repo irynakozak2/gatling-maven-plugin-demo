1be8bb53a28ca01291bee8e4d6856ba390c4657f
irynakozak2 <Iryna_Kozak2@epam.com>
2021-07-29 17:52:44 +0300

    Add logic for generation ES Logs

--- src/test/scala/computerdatabase/BasicSimulation.scala
+++ src/test/scala/computerdatabase/BasicSimulation.scala
@@ -1,10 +1,52 @@
 package computerdatabase
 
+import java.text.SimpleDateFormat
+import java.util.Date
+
+import scala.util.Random
+import io.gatling.core
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
+
 import scala.concurrent.duration._
+import io.gatling.core.feeder._
+import io.gatling.core.structure.ChainBuilder
 
 class BasicSimulation extends Simulation {
+
+
+  val stringCharsSeq = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+  val intCharsSeq = "123456789"
+
+  def getRandomString(n:Int) = (1 to n).map(_ => stringCharsSeq(Random.nextInt(stringCharsSeq.length))).mkString
+  def getRandomInt(n:Int) = (1 to n).map(_ => intCharsSeq(Random.nextInt(intCharsSeq.length))).mkString
+  def timeFormatForES() = {
+    new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date).toString
+  }
+
+  val createESLogTest = exec(http("Create ES Log")
+    .post("http://35.180.22.52:32510/index_logs")
+    .body(StringBody(session => session("generate_eslogs_batch_json").as[String])).asJson
+  )
+
+  def generateJsonForBatchESLogs: ChainBuilder = {
+    exec(session => {
+      var jsonBody: String = """{"logs": ["""
+      for(i <- 1 until 10) {
+        val x_variable = "log_message" + i
+        val tmpLog = session(s"${x_variable}").as[String]
+        jsonBody ++= s"""{\"uuid\": \"${getRandomString(36)}\", \"log_time\": \"${timeFormatForES()}\", \"log_message\": \"${tmpLog}\", \"item_id\": ${getRandomInt(5)}, \"launch_id\": ${getRandomInt(5)}, \"last_modified\": \"${timeFormatForES()}\", \"log_level\": ${getRandomInt(5)}, \"attachment_id\": ${getRandomInt(5)}}"""
+        if(i + 1 != 10){
+          jsonBody ++= ","
+        }
+      }
+      jsonBody ++= """], "project": 9575}"""
+      session.set("generate_eslogs_batch_json", jsonBody)
+    })
+  }
+
+  val logMessage = csv("log_messages_50testrows.csv").batch(200).queue
+  val logsCount = logMessage.readRecords.size
 
   val httpProtocol = http
     .baseUrl("http://computer-database.gatling.io") // Here is the root for all relative URLs
@@ -13,40 +55,12 @@ class BasicSimulation extends Simulation {
     .acceptLanguageHeader("en-US,en;q=0.5")
     .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20100101 Firefox/16.0")
 
-  val scn = scenario("Scenario Name") // A scenario is a chain of requests and pauses
-    .exec(http("request_1")
-      .get("/"))
-    .pause(7) // Note that Gatling has recorder real time pauses
-    .exec(http("request_2")
-      .get("/computers?f=macbook"))
-    .pause(2)
-    .exec(http("request_3")
-      .get("/computers/6"))
-    .pause(3)
-    .exec(http("request_4")
-      .get("/"))
-    .pause(2)
-    .exec(http("request_5")
-      .get("/computers?p=1"))
-    .pause(670.milliseconds)
-    .exec(http("request_6")
-      .get("/computers?p=2"))
-    .pause(629.milliseconds)
-    .exec(http("request_7")
-      .get("/computers?p=3"))
-    .pause(734.milliseconds)
-    .exec(http("request_8")
-      .get("/computers?p=4"))
-    .pause(5)
-    .exec(http("request_9")
-      .get("/computers/new"))
-    .pause(1)
-    .exec(http("request_10") // Here's an example of a POST request
-      .post("/computers")
-      .formParam("""name""", """Beautiful Computer""") // Note the triple double quotes: used in Scala for protecting a whole chain of characters (no need for backslash)
-      .formParam("""introduced""", """2012-05-30""")
-      .formParam("""discontinued""", """""")
-      .formParam("""company""", """37"""))
+  val scn = scenario("Scenario Name")
+    .during(30.seconds, exitASAP = false) {
+      feed(logMessage,10)
+        .exec(generateJsonForBatchESLogs)
+        .exec(createESLogTest)
+    }
 
-  setUp(scn.inject(atOnceUsers(1)).protocols(httpProtocol))
+  setUp(scn.inject(rampUsers(1).during(30.seconds)).protocols(httpProtocol))
 }
